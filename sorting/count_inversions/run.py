9b7c69ed1fd3d46caf9956029b17cf4ebbdc84d4
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-06-12 01:07:47 +0600

    sorting/count_inversions

--- sorting/count_inversions/run.py
+++ sorting/count_inversions/run.py
@@ -1,34 +1,34 @@
-# O(nlog(n)) time | O(n) space --> inplace sorting
-def mergeSort(array):
-    if len(array) == 1:
-        return array
+# O(nlog(n)) time | O(n) space
+def countInversions(array):
+    if len(array) <= 1:
+        return 0
     auxiliary = array[:]
-    helper(array, 0, len(array) - 1, auxiliary)
-    return array
+    return helper(array, 0, len(array) - 1, auxiliary)
 
 
 def helper(array, start, end, auxiliary):
     if start == end:
-        return
+        return 0
     mid = (start + end) // 2
-    helper(auxiliary, start, mid, array)
-    helper(auxiliary, mid + 1, end, array)
-    do_merge(array, start, mid, end, auxiliary)
+    left = helper(auxiliary, start, mid, array)
+    right = helper(auxiliary, mid + 1, end, array)
+    return left + right + do_merge(array, start, mid, end, auxiliary)
 
 
 def do_merge(array, start, mid, end, auxiliary):
     k = start
     i = start
     j = mid + 1
+    inversions = 0
     while i <= mid and j <= end:
         if auxiliary[i] <= auxiliary[j]:
             array[k] = auxiliary[i]
             i += 1
-            k += 1
         else:
             array[k] = auxiliary[j]
+            inversions += mid - i + 1
             j += 1
-            k += 1
+        k += 1
     while i <= mid:
         array[k] = auxiliary[i]
         i += 1
@@ -37,9 +37,4 @@ def do_merge(array, start, mid, end, auxiliary):
         array[k] = auxiliary[j]
         j += 1
         k += 1
-
-
-if __name__ == '__main__':
-    data = [8, 5, 2, 9, 5, 6, 3]
-    mergeSort(data)
-    print(data)
+    return inversions
