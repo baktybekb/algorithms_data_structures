fd702fa90ce4c0995ec0ab176bd346094c303284
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-06-26 03:48:05 +0600

    data_structures/hash_table

--- data_structures/hash_table/performance_test.py
+++ data_structures/hash_table/performance_test.py
@@ -1,35 +1,28 @@
 import multiprocessing
+
 from data_structures.hash_table.ds import HashTable
 from data_structures.memory_usage import memory_usage
 
 default_range = 1000000
 
 
-def hash_table_put():
-    table = HashTable(5)
-    for i in range(default_range):
-        table.put(i, i)
-
-
-def dict_put():
-    table = dict()
+def hash_table_put(instance=None):
+    table = HashTable() if instance else dict()
     for i in range(default_range):
         table[i] = i
 
 
 def performance_test():
-    process = multiprocessing.Process(target=hash_table_put)
-    measurements, max_usage = memory_usage(process)
+    process = multiprocessing.Process(target=hash_table_put, args=(HashTable,))
+    _, max_usage = memory_usage(process)
     print(f'HashTable `put` memory usage(max): {max_usage} MiB')
-    process = multiprocessing.Process(target=dict_put)
-    measurements, max_usage = memory_usage(process)
+    process = multiprocessing.Process(target=hash_table_put)
+    _, max_usage = memory_usage(process)
     print(f'Python dict `put` memory usage(max): {max_usage} MiB')
-    print('-' * 50)
 
     """
-    HashTable `put` memory usage(max): 507.25 MiB
-    Python dict `put` memory usage(max): 116.5 MiB
-    --------------------------------------------------
+    HashTable `put` memory usage(max): 212.390625 MiB
+    Python dict `put` memory usage(max): 118.546875 MiB
     """
 
 
