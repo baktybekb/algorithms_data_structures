59ba221712959326f1311a3582ec60fea92f1ada
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-07-01 21:03:40 +0600

    graphs/has_single_cycle

---
+++ algoexpert/graphs/single_cycle_check.py
@@ -0,0 +1,9 @@
+def hasSingleCycle(array):
+    visited = 0
+    idx = 0
+    while visited < len(array):
+        if visited > 1 and idx == 0:
+            return False
+        idx = (idx + array[idx]) % len(array)
+        visited += 1
+    return idx == 0
