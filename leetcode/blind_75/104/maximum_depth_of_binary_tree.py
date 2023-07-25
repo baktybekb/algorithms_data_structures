af760f7e069ad89b38956e7e63f47220981cc24c
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-07-25 20:33:16 +0600

    leetcode/same_tree

--- leetcode/blind_75/104/maximum_depth_of_binary_tree.py
+++ leetcode/blind_75/104/maximum_depth_of_binary_tree.py
@@ -1,14 +1,14 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
     # O(n) time complexity | O(d), d - depth of the tree
-    def maxDepth(self, root, depth=0) -> int:
+    def maxDepth(self, root: TreeNode = None, depth=0) -> int:
         if root is None:
             return depth
         return max(
