dd890f92af0bab5e4fa9ee7b5b427d90ef6777e8
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-07-27 20:33:38 +0600

    leetcode/208_implement_trie

---
+++ leetcode/blind_75/tree/208_implement_trie.py
@@ -0,0 +1,36 @@
+class Trie:
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = '*'
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node[self.end_symbol] = word
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return True if self.end_symbol in node else False
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
