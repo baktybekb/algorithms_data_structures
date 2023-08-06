99508bf41bf1f80cc3926dccf4658a5d7ba9beec
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-08-06 18:26:51 +0600

    leetcode/blind_75/graph/200_number_of_islands

---
+++ leetcode/blind_75/graph/200_number_of_islands.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            grid[r][c] = '0'
+            for r_, c_ in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                new_r, new_c = r + r_, c + c_
+                if not 0 <= new_r < rows or not 0 <= new_c < cols:
+                    continue
+                if grid[new_r][new_c] == '0':
+                    continue
+                dfs(new_r, new_c)
+
+        count = 0
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == '0':
+                    continue
+                dfs(row, col)
+                count += 1
+        return count
