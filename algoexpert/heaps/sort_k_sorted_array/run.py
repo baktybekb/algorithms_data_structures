1e38ed52e1ec3774ee9bd72aace5d419d9a6387d
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-08-11 17:03:36 +0600

    heaps

--- algoexpert/heaps/sort_k_sorted_array/run.py
+++ algoexpert/heaps/sort_k_sorted_array/run.py
@@ -1,8 +1,8 @@
-# O(nlog(k)) time | O(k) space, where k ~ length of min heap, n - numbers in the array
+# O(n * log(k)) time | O(k) space
 def sortKSortedArray(array, k):
+    heap = Heap(array[:k + 1])
     index = 0
-    heap = MinHeap(array[:k + 1])
-    while heap.length > 0:
+    while len(heap):
         array[index] = heap.remove()
         index += 1
         if index + k < len(array):
@@ -10,39 +10,21 @@ def sortKSortedArray(array, k):
     return array
 
 
-class MinHeap:
-    def __init__(self, array, ):
-        self.heap = self.build_heap(array)
-        self.length = len(self.heap)
+class Heap:
+    def __init__(self, array):
+        self.heap = self.buildHeap(array)
 
-    # O(n) time, because we us sift_down() --> starting from the bottom parents
-    # O(nlog(n)), when we use sift_up() for building --> starting from the top
-    def build_heap(self, array):
-        last_parent_idx = (len(array) - 2) // 2
+    def buildHeap(self, array):
+        last_parent_idx = len(array) - 2 // 2
         for idx in reversed(range(last_parent_idx + 1)):
-            self.sift_down(idx, len(array) - 1, array)
+            self.siftDown(array, idx)
         return array
 
-    def sift_up(self, current_idx, array):
-        parent_idx = (current_idx - 1) // 2
-        while current_idx > 0 and array[current_idx] < array[parent_idx]:
-            self.swap(array, parent_idx, current_idx)
-            current_idx = parent_idx
-            parent_idx = (current_idx - 1) // 2
+    def __len__(self):
+        return len(self.heap)
 
-    def insert(self, value):
-        self.heap.append(value)
-        self.sift_up(len(self.heap) - 1, self.heap)
-        self.length += 1
-
-    def remove(self):
-        self.swap(self.heap, 0, len(self.heap) - 1)
-        value_to_remove = self.heap.pop()
-        self.sift_down(0, len(self.heap) - 1, self.heap)
-        self.length -= 1
-        return value_to_remove
-
-    def sift_down(self, current_idx, end_idx, array):
+    def siftDown(self, array, current_idx):
+        end_idx = len(array) - 1
         child_one_idx = current_idx * 2 + 1
         while child_one_idx <= end_idx:
             child_two_idx = current_idx * 2 + 2 if current_idx * 2 + 2 <= end_idx else -1
@@ -56,6 +38,27 @@ class MinHeap:
                 child_one_idx = current_idx * 2 + 1
             else:
                 break
+
+    def siftUp(self):
+        current_idx = len(self.heap) - 1
+        parent_idx = (current_idx - 1) // 2
+        while current_idx > 0 and self.heap[current_idx] < self.heap[parent_idx]:
+            self.swap(self.heap, parent_idx, current_idx)
+            current_idx = parent_idx
+            parent_idx = (current_idx - 1) // 2
+
+    def peek(self):
+        return self.heap[0]
+
+    def remove(self):
+        self.swap(self.heap, 0, len(self.heap) - 1)
+        value = self.heap.pop()
+        self.siftDown(self.heap, 0)
+        return value
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.siftUp()
 
     def swap(self, array, i, j):
         array[i], array[j] = array[j], array[i]
