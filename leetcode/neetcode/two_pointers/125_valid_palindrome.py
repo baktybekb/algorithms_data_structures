7d9e1189cc5d0686a98f8a3f6200362dab23a116
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-09-14 22:44:30 +0600

    leetcode/neetcode/125_valid_palindrome

--- leetcode/neetcode/two_pointers/125_valid_palindrome.py
+++ leetcode/neetcode/two_pointers/125_valid_palindrome.py
@@ -1,12 +1,10 @@
-# O(n) time | O(1) space
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l = 0
-        r = len(s) - 1
+        l, r = 0, len(s) - 1
         while l < r:
-            while l < r and not self.is_alfanum(s[l]):
+            while l < r and not self.is_alphanum(s[l]):
                 l += 1
-            while l < r and not self.is_alfanum(s[r]):
+            while l < r and not self.is_alphanum(s[r]):
                 r -= 1
             if s[l].lower() != s[r].lower():
                 return False
@@ -14,9 +12,9 @@ class Solution:
             r -= 1
         return True
 
-    def is_alfanum(self, char):
+    def is_alphanum(self, char):
         return (
-                ord('A') <= ord(char) <= ord('Z') or
                 ord('a') <= ord(char) <= ord('z') or
+                ord('A') <= ord(char) <= ord('Z') or
                 ord('0') <= ord(char) <= ord('9')
         )
