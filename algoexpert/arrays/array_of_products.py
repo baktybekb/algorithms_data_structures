1fd230914797557c0ad20bf7333e232faa8a35a9
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-09-25 17:58:56 +0600

    algoexpert/arrays/array_of_products

---
+++ algoexpert/arrays/array_of_products.py
@@ -0,0 +1,13 @@
+# O(n) time | O(n) space
+def arrayOfProducts(array):
+    res = [1] * len(array)
+    multiplier = 1
+    for i in range(len(array)):
+        res[i] *= multiplier
+        multiplier *= array[i]
+    multiplier = 1
+    for i in reversed(range(len(array))):
+        res[i] *= multiplier
+        multiplier *= array[i]
+    return res
+
