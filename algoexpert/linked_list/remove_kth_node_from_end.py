2eb0d182687f2f3da12f7cdade05fa4e9fa87644
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-09-30 23:09:05 +0600

    algoexpert/linked_list/remove_kth_node_from_end

---
+++ algoexpert/linked_list/remove_kth_node_from_end.py
@@ -0,0 +1,21 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# O(n) time | O(1) space
+def removeKthNodeFromEnd(head, k):
+    l = r = head
+    for i in range(k):
+        r = r.next
+    if r is None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return head
+    while r and r.next:
+        l = l.next
+        r = r.next
+    l.next = l.next.next
+    return head
