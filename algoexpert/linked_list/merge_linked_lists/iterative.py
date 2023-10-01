79b5c4f6cd0b61efb6014987f0697c90b3f0124a
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-10-01 20:11:07 +0600

    algoexpert/linked_list/merge_linked_lists

--- algoexpert/linked_list/merge_linked_lists/iterative.py
+++ algoexpert/linked_list/merge_linked_lists/iterative.py
@@ -1,3 +1,5 @@
+# https://www.algoexpert.io/questions/merge-linked-lists
+
 # This is an input class. Do not edit.
 class LinkedList:
     def __init__(self, value):
@@ -7,24 +9,18 @@ class LinkedList:
 
 # O(n + m) time | O(1) space
 def mergeLinkedLists(headOne, headTwo):
+    one, two = headOne, headTwo
     prev = None
-    p1 = headOne
-    p2 = headTwo
-    while p1 and p2:
-        if p1.value > p2.value:
+    while one and two:
+        if one.value <= two.value:
             if prev:
-                prev.next = p2
-            prev = p2
-            p2 = p2.next
-            prev.next = p1
+                prev.next = one
+            prev = one
+            one = one.next
         else:
-            prev = p1
-            p1 = p1.next
-    if p1 is None:
-        prev.next = p2
-    if headOne.value <= headTwo.value:
-        return headOne
-    return headTwo
-
-
-
+            if prev:
+                prev.next = two
+            prev = two
+            two = two.next
+    prev.next = one if one else two
+    return headOne if headOne.value <= headTwo.value else headTwo
