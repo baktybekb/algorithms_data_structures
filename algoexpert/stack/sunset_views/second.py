83435f388bc21dc38016400991c90ca4dcd6dd3e
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-10-02 13:43:30 +0600

    algoexpert/stack/sunset_views

--- algoexpert/stack/sunset_views/second.py
+++ algoexpert/stack/sunset_views/second.py
@@ -1,30 +1,21 @@
 # O(n) time | O(n) space
 def sunsetViews(buildings, direction):
-    idx = 0 if direction == 'EAST' else len(buildings) - 1
-    step = 1 if direction == 'EAST' else -1
-    stack = []
-    while 0 <= idx < len(buildings):
-        while stack and buildings[stack[-1]] <= buildings[idx]:
-            stack.pop()
-        stack.append(idx)
-        idx += step
-    if direction == 'EAST':
-        return stack
-    return reverse(stack)
+    res = []
+    cur_range = range(len(buildings))
+    traverse = cur_range if direction == 'WEST' else reversed(cur_range)
+    max_val = 0
+    for i in traverse:
+        if buildings[i] > max_val:
+            max_val = buildings[i]
+            res.append(i)
+    return res if direction == 'WEST' else sort_array(res)
 
 
-def reverse(array):
+# O(n) time | O(1) space
+def sort_array(array):
     l, r = 0, len(array) - 1
-    while l <= r:
+    while l < r:
         array[l], array[r] = array[r], array[l]
         l += 1
         r -= 1
     return array
-
-
-if __name__ == '__main__':
-    res = sunsetViews(buildings=[3, 5, 4, 4, 3, 1, 3, 2], direction='EAST')
-    assert res == [1, 3, 6, 7]
-
-    res = sunsetViews(buildings=[3, 5, 4, 4, 3, 1, 3, 2], direction='WEST')
-    assert res == [0, 1]
