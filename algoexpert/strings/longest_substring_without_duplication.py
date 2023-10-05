da7f2b727cdf28e2c9f44bf402317cdf45e16625
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-10-05 22:34:43 +0600

    algoexpert/strings/longest_substring_without_duplication

---
+++ algoexpert/strings/longest_substring_without_duplication.py
@@ -0,0 +1,21 @@
+# https://www.algoexpert.io/questions/longest-substring-without-duplication
+
+# O(n) time | O(n) space
+def longestSubstringWithoutDuplication(string):
+    sub_set = set()
+    l = 0
+    res_l = res_r = 0
+    for r in range(len(string)):
+        while string[r] in sub_set:
+            sub_set.remove(string[l])
+            l += 1
+        sub_set.add(string[r])
+        if r - l > res_r - res_l:
+            res_l, res_r = l, r
+    return string[res_l:res_r + 1]
+
+
+if __name__ == '__main__':
+    assert longestSubstringWithoutDuplication("clementisacap") == 'mentisac'
+    assert longestSubstringWithoutDuplication("abc") == 'abc'
+    assert longestSubstringWithoutDuplication("abcbde") == 'cbde'
