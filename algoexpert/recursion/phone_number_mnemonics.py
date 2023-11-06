e74d0fa223d7c39fc82f2f7340abc5261d9c0da3
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-11-07 02:38:06 +0600

    recursion/phone_number_mnemonics

--- algoexpert/recursion/phone_number_mnemonics.py
+++ algoexpert/recursion/phone_number_mnemonics.py
@@ -1,33 +1,31 @@
+# https://www.algoexpert.io/questions/phone-number-mnemonics
+
 # O(4^n * n) time | O(4^n * n) space
 def phoneNumberMnemonics(phoneNumber):
-    table = {
-        '0': ('0',),
-        '1': ('1',),
-        '2': ('a', 'b', 'c'),
-        '3': ('d', 'e', 'f'),
-        '4': ('g', 'h', 'i'),
-        '5': ('j', 'k', 'l'),
-        '6': ('m', 'n', 'o'),
-        '7': ('p', 'q', 'r', 's'),
-        '8': ('t', 'u', 'v'),
-        '9': ('w', 'x', 'y', 'z'),
+    res, subset = [], []
+    number_map = {
+        '0': '0',
+        '1': '1',
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
     }
-    result = []
-    current = [None] * len(phoneNumber)
-    pick_char(table, result, 0, phoneNumber, current)
-    return result
 
+    def helper(i):
+        if i == len(phoneNumber):
+            res.append(''.join(subset))
+            return
+        options = number_map[phoneNumber[i]]
+        for option in options:
+            subset.append(option)
+            helper(i + 1)
+            subset.pop()
 
-def pick_char(table, result, idx, phone_number, current):
-    if idx >= len(phone_number):
-        result.append(''.join(current))
-        return
-    chars = table[phone_number[idx]]
-    for i in range(len(chars)):
-        current[idx] = chars[i]
-        pick_char(table, result, idx + 1, phone_number, current)
+    helper(0)
+    return res
 
-
-if __name__ == '__main__':
-    res = phoneNumberMnemonics('79')
-    assert res == ['pw', 'px', 'py', 'pz', 'qw', 'qx', 'qy', 'qz', 'rw', 'rx', 'ry', 'rz', 'sw', 'sx', 'sy', 'sz']
