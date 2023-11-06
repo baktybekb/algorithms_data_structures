609c29e2a3a3fb558084fa27e0dfb2813a9c4ebd
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-11-06 19:45:55 +0600

    recursion/product_sum

--- algoexpert/recursion/product_sum.py
+++ algoexpert/recursion/product_sum.py
@@ -1,11 +1,11 @@
-# Tip: You can use the type(element) function to check whether an item
-# is a list or an integer.
-# O(n) time | O(d) space, depth of nested arrays in array
+# https://www.algoexpert.io/questions/product-sum
+
+# O(n) time | O(d) space, d --> max depth of nested lists
 def productSum(array, depth=1):
     total = 0
-    for item in array:
-        if type(item) == list:
-            total += productSum(item, depth + 1)
+    for val in array:
+        if isinstance(val, list):
+            total += productSum(val, depth + 1)
         else:
-            total += item
-    return total * depth
+            total += val
+    return depth * total
