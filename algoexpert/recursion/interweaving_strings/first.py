b13c3d36020bc49bcffa7db0ed8015d61077ebb7
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-11-07 19:57:33 +0600

    recursion/interweaving_strings

--- algoexpert/recursion/interweaving_strings/first.py
+++ algoexpert/recursion/interweaving_strings/first.py
@@ -1,26 +1,21 @@
-# O(2^(m + n)), m, n - length of one and two, respectively
-# O(n + m) space, recursion depth
+# O(2^(n + m)) time | O(n + m) time
 def interweavingStrings(one, two, three):
     if len(three) != len(one) + len(two):
         return False
-    counter = [0]
-    res = helper(one, two, three, 0, 0, counter)
-    return res
 
-
-def helper(one, two, three, i, j, counter):
-    counter[0] += 1
-    k = i + j
-    if k == len(three):
-        return True
-    if i < len(one) and one[i] == three[k]:
-        if helper(one, two, three, i + 1, j, counter):
+    def helper(i, j):
+        k = i + j
+        if k == len(three):
             return True
-    if j < len(two) and two[j] == three[k]:
-        return helper(one, two, three, i, j + 1, counter)
-    return False
+        if i < len(one) and one[i] == three[k]:
+            if helper(i + 1, j):
+                return True
+        if j < len(two) and two[j] == three[k]:
+            return helper(i, j + 1)
+        return False
+
+    return helper(0, 0)
 
 
 if __name__ == '__main__':
-    # 520 recursion calls
-    interweavingStrings(one="aaaaaaaa", two="aaaaaaaab", three="aaaaaaaabaaaaaaaa")
+    interweavingStrings(one="aaaaaa", two="aaaaaab", three="aaaaaabaaaaaa")
