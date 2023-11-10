fadeccff1e33cb232258170fbbf9adcb5d270a51
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-11-10 08:19:37 +0600

    graphs/single_cycle_check

--- algoexpert/graphs/single_cycle_check.py
+++ algoexpert/graphs/single_cycle_check.py
@@ -1,10 +1,15 @@
+# https://www.algoexpert.io/questions/single-cycle-check
+
+# 1. we start from 0 index
+# 2.if after traversing the array idx != 0 --> we must do only one cycle, and return to idx ==0
+# 3. if 0 < i < len(array) and idx == 0 --> cycle during the loop
+
 # O(n) time | O(1) space
 def hasSingleCycle(array):
-    visited = 0
-    idx = 0
-    while visited < len(array):
-        if visited > 1 and idx == 0:
+    dest_idx = 0
+    for i in range(len(array)):
+        if i > 0 and dest_idx == 0:
             return False
-        idx = (idx + array[idx]) % len(array)
-        visited += 1
-    return idx == 0
+        step = array[dest_idx]
+        dest_idx = (dest_idx + step) % len(array)
+    return dest_idx == 0
