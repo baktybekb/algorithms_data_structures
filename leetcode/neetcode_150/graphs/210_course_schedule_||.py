685ade9c460cc510eda1238d9c15cc266c9748c6
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-11-12 06:56:22 +0600

    leetcode/graphs/210_course_schedule_||

--- leetcode/neetcode_150/graphs/210_course_schedule_||.py
+++ leetcode/neetcode_150/graphs/210_course_schedule_||.py
@@ -1,27 +1,26 @@
-# https://leetcode.com/problems/course-schedule/description/
-
 from typing import List
 
 
 # O(v + e) time | O(v) space, v --> number of courses
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = [[] for _ in range(numCourses)]
         for index, prereq in prerequisites:
             graph[index].append(prereq)
-        stage = [0] * numCourses
+        stage, order = [0] * numCourses, []
 
         def dfs(index):
-            if stage[index]:
+            if stage[index] == 2:
                 return True
             stage[index] = 1
             for prereq in graph[index]:
                 if stage[prereq] == 1 or not dfs(prereq):
                     return False
             stage[index] = 2
+            order.append(index)
             return True
 
         for index in range(len(graph)):
             if not dfs(index):
-                return False
-        return True
+                return []
+        return order
