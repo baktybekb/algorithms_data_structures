43b630b4067f24e0d782615a660ef469968d85bd
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-11-30 04:48:49 +0600

    outtalent/goals/leetcode

---
+++ outtalent/goals/leetcode/482_license_key_formatting.py
@@ -0,0 +1,7 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        pass
+
+
+if __name__ == '__main__':
+    Solution().licenseKeyFormatting("5F3Z-2e-9-w", 4)
---
+++ outtalent/goals/leetcode/500_keyboard_row.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+# O(n * m) time | O(n) space, n - len(words), m - length of the longest word
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        data = ('qwertyuiop', 'asdfghjkl', 'zxcvbnm')
+        char_map = {}
+        for i in range(len(data)):
+            for char in data[i]:
+                char_map[char] = i
+        res = []
+        for word in words:
+            if all(char_map[word[0].lower()] == char_map[char.lower()] for char in word):
+                res.append(word)
+        return res
+
