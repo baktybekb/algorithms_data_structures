dd6bf0a0ce3b8fbc38c116d1f6c8e5b553fafebc
bbaiserkeev <bbaiserkeev@s-pro.io>
2024-02-10 18:21:42 +0600

    algoexpert/dynamic_programming

--- algoexpert/dynamic_programming/max_subset_sum_no_adjacent.py
+++ algoexpert/dynamic_programming/max_subset_sum_no_adjacent.py
@@ -1,8 +1,15 @@
-# O(n) time | O(1) space
+# O(n) time | O(n) space
 def maxSubsetSumNoAdjacent(array):
-    prev = current = 0
+    if not array:
+        return 0
+    sum_array = [0] * len(array)
     for i in range(len(array)):
-        temp = current
-        current = max(prev + array[i], current)
-        prev = temp
-    return current
+        prev = 0 if i - 2 < 0 else sum_array[i - 2]
+        current = 0 if i - 1 < 0 else sum_array[i - 1]
+        sum_array[i] = max(current, prev + array[i])
+    return sum_array[-1]
+
+
+if __name__ == '__main__':
+    array = [75, 105, 120, 75, 90, 135]
+    assert maxSubsetSumNoAdjacent(array) == 330
